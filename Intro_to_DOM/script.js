42b5f2263c9d846bc7e107e4ea65585ddf288f34
shwoods-coder <132965079+shwoods-coder@users.noreply.github.com>
2025-04-07 00:21:08 -0700

    Updated

--- Intro_to_DOM/script.js
+++ Intro_to_DOM/script.js
@@ -1,4 +1,23 @@
 let titleElement = document.getElementById("title");
 
+let practiceElement = document.getElementById("practice");
+
 console.log(titleElement);
+console.log (practiceElement);
+
+const heading= document.querySelector("#mainHeading");
+const paragraph = document.querySelectorAll ("p");
+const button = document.querySelector ("button");
+
+button.addEventListener("click", function() {
+    heading.innerHTML = "No, Friend!";
+    paragraph.innerText = "We said don't push.";
+    button.innerHTML = "Failed!";
+});
+
+
+console.log(heading);
+console.log(paragraph);
+console.log(button);
+
 
